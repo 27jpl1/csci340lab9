d40dbe0933ed7ebcb0c1b39ca352f14bae4d44d9
JP Lyon <japylyon@gmail.com>
2023-10-27 16:13:02 -0500

    Steps 3 and 4

--- LyonCollege/Pages/Students/Index.cshtml.cs
+++ LyonCollege/Pages/Students/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using LyonCollege.Models;
@@ -11,22 +12,70 @@ using LyonCollege.Data;
 namespace LyonCollege.Pages.Students
 {
     public class IndexModel : PageModel
+{
+    private readonly SchoolContext _context;
+    private readonly IConfiguration Configuration;
+    public IndexModel(SchoolContext context, IConfiguration configuration)
     {
-        private readonly LyonCollege.Data.SchoolContext _context;
-
-        public IndexModel(LyonCollege.Data.SchoolContext context)
-        {
-            _context = context;
-        }
-
-        public IList<Student> Student { get;set; } = default!;
-
-        public async Task OnGetAsync()
-        {
-            if (_context.Students != null)
-            {
-                Student = await _context.Students.ToListAsync();
-            }
-        }
+        _context = context;
+        Configuration = configuration;
     }
+
+    public string NameSort { get; set; }
+    public string DateSort { get; set; }
+    public string AgeSort { get; set; }
+    public string CurrentFilter { get; set; }
+    public string CurrentSort { get; set; }
+    public PaginatedList<Student> Students { get; set; }
+    public async Task OnGetAsync(string sortOrder,
+    string currentFilter, string searchString, int? pageIndex)
+    {
+        CurrentSort = sortOrder;
+        NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+        DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+        AgeSort = sortOrder == "Age" ? "age_desc" : "Age";
+        if (searchString != null)
+        {
+            pageIndex = 1;
+        }
+        else
+        {
+            searchString = currentFilter;
+        }
+        CurrentFilter = searchString;
+
+        IQueryable<Student> studentsIQ = from s in _context.Students
+                                         select s;
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
+                                   || s.FirstMidName.Contains(searchString));
+        }
+
+        switch (sortOrder)
+        {
+            case "name_desc":
+                studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                break;
+            case "Date":
+                studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                break;
+            case "date_desc":
+                studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                break;
+            case "Age":
+                studentsIQ = studentsIQ.OrderBy(s => s.Age);
+                break;
+            case "age_desc":
+                studentsIQ = studentsIQ.OrderByDescending(s => s.Age);
+                break;
+            default:
+                studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                break;
+        }
+        var pageSize = Configuration.GetValue("PageSize", 4);
+        Students = await PaginatedList<Student>.CreateAsync(
+            studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+    }
+}
 }
