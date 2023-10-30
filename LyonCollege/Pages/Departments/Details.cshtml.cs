8d93ed93f33a4ffa38aec6726d511704251eeaee
JP Lyon <japylyon@gmail.com>
2023-10-30 14:08:06 -0500

    Step 8

---
+++ LyonCollege/Pages/Departments/Details.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LyonCollege.Data;
+using LyonCollege.Models;
+
+namespace LyonCollege.Pages.Departments
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly LyonCollege.Data.SchoolContext _context;
+
+        public DetailsModel(LyonCollege.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+      public Department Department { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Departments == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Departments.FirstOrDefaultAsync(m => m.DepartmentID == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Department = department;
+            }
+            return Page();
+        }
+    }
+}
